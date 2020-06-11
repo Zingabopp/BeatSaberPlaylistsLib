2d6b1953643f2ba6c643cae5ddac3ff621d7dc79
Zingabopp <Zingabopp@gmail.com>
2020-06-11 18:13:36 -0500

    Tests for PlaylistManager.GetSupportedExtensions

---
+++ BeatSyncPlaylistLibTests/PlaylistManager_Tests/GetSupportedExtensions_Tests.cs
@@ -0,0 +1,22 @@
+﻿using BeatSaberPlaylistsLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BeatSaberPlaylistsLibTests.PlaylistManager_Tests
+{
+    [TestClass]
+    public class GetSupportedExtensions_Tests
+    {
+        private static readonly string OutputPath = Path.Combine(TestTools.OutputFolder, "GetSupportedExtensions");
+        [TestMethod]
+        public void Constructor()
+        {
+            string playlistsPath = Path.Combine(OutputPath, "Constructor");
+            PlaylistManager manager = new PlaylistManager(playlistsPath);
+            Assert.IsTrue(manager.GetSupportedExtensions().Length > 0);
+        }
+    }
+}

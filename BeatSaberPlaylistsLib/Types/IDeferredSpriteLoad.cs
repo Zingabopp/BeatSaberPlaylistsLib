6f0eb2eedefe8d1118963b21400df17ca01e79fd
Zingabopp <Zingabopp@gmail.com>
2020-08-06 11:47:52 -0500

    Interface for deferred sprite load, only load once

---
+++ BeatSaberPlaylistsLib/Types/IDeferredSpriteLoad.cs
@@ -0,0 +1,16 @@
+﻿#if BeatSaber
+extern alias BeatSaber;
+using BeatSaber::UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeatSaberPlaylistsLib.Types
+{
+    public interface IDeferredSpriteLoad
+    {
+        event EventHandler? SpriteLoaded;
+        Sprite? Sprite { get; }
+    }
+}
+#endif

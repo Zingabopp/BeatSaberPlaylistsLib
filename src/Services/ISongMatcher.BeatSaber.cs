3bf4130f0b64caa89e3ba9655a3609abc217fff1
Zingabopp <Zingabopp.git.noreply@gmail.com>
2021-06-28 23:04:46 -0500

    Initial idea for song matching service

---
+++ src/Services/ISongMatcher.BeatSaber.cs
@@ -0,0 +1,26 @@
+﻿#if BeatSaber
+extern alias BeatSaber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BeatSaberPlaylistsLib.Types;
+
+namespace BeatSaberPlaylistsLib.Services
+{
+    public interface ISongMatcher
+    {
+        bool MatchSong(ISong song);
+    }
+
+    public class SongCoreMatcher : ISongMatcher
+    {
+        public bool MatchSong(ISong song)
+        {
+
+            var _previewBeatmapLevel = SongCore.Loader.GetLevelById(song.LevelId);
+            song.SetPreviewBeatmap(_previewBeatmapLevel);
+            return _previewBeatmapLevel != null;
+        }
+    }
+}
+#endif

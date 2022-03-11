9d5bc8a22fbace20a33af3c0bc593de3adab1036
PixelBoom <rithikbhatia12@gmail.com>
2022-03-11 11:23:09 -0500

    Changed collage size to 256x256

--- Shared/ImageUtilities.cs
+++ Shared/ImageUtilities.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
@@ -12,6 +11,8 @@ namespace BeatSaberPlaylistsLib
     /// </summary>
     public static class ImageUtilities
     {
+        public const int kImageSize = 256;
+        
         /// <summary>
         /// Generate a collage of 2 images
         /// </summary>
@@ -20,7 +21,7 @@ namespace BeatSaberPlaylistsLib
         /// <returns></returns>
         public static async Task<Stream> GenerateCollage(Stream imageStream1, Stream imageStream2)
         {
-            var image = new Image<Rgba32>(512, 512);
+            var image = new Image<Rgba32>(kImageSize, kImageSize);
             using var image1 = await Image.LoadAsync(imageStream1);
             using var image2 = await Image.LoadAsync(imageStream2);
 
@@ -30,23 +31,26 @@ namespace BeatSaberPlaylistsLib
                 {
                     image1.Mutate(i1 =>
                     {
-                        i1.Resize(512, 512);
-                        i1.Crop(new Rectangle(64, 0, 256, 512));
+                        i1.Resize(kImageSize, kImageSize);
+                        i1.Crop(new Rectangle(kImageSize / 8, 0, kImageSize / 2, kImageSize));
                     });
                     i.DrawImage(image1, Point.Empty, 1.0f);
 
                     image2.Mutate(i2 =>
                     {
-                        i2.Resize(512, 512);
-                        i2.Crop(new Rectangle(64, 0, 256, 512));
+                        i2.Resize(kImageSize, kImageSize);
+                        i2.Crop(new Rectangle(kImageSize / 8, 0, kImageSize / 2, kImageSize));
                     });
-                    i.DrawImage(image2, new Point(256, 0), 1.0f);
+                    i.DrawImage(image2, new Point(kImageSize / 2, 0), 1.0f);
                 });
             });
 
             var imageStream = new MemoryStream();
-            await image.SaveAsync(imageStream, new PngEncoder());
-            imageStream.Position = 0;
+            await image.SaveAsPngAsync(imageStream);
+            if (imageStream.CanSeek)
+            {
+                imageStream.Seek(0, SeekOrigin.Begin);
+            }
             return imageStream;
         }
         
@@ -59,7 +63,7 @@ namespace BeatSaberPlaylistsLib
         /// <returns></returns>
         public static async Task<Stream> GenerateCollage(Stream imageStream1, Stream imageStream2, Stream imageStream3)
         {
-            var image = new Image<Rgba32>(512, 512);
+            var image = new Image<Rgba32>(kImageSize, kImageSize);
             using var image1 = await Image.LoadAsync(imageStream1);
             using var image2 = await Image.LoadAsync(imageStream2);
             using var image3 = await Image.LoadAsync(imageStream3);
@@ -70,28 +74,31 @@ namespace BeatSaberPlaylistsLib
                 {
                     image1.Mutate(i1 =>
                     {
-                        i1.Resize(512, 512);
-                        i1.Crop(new Rectangle(0, 64, 512, 256));
+                        i1.Resize(kImageSize, kImageSize);
+                        i1.Crop(new Rectangle(0, kImageSize / 8, kImageSize, kImageSize / 2));
                     });
                     i.DrawImage(image1, Point.Empty, 1.0f);
 
                     image2.Mutate(i2 =>
                     {
-                        i2.Resize(256, 256);
+                        i2.Resize(kImageSize / 2, kImageSize / 2);
                     });
-                    i.DrawImage(image2, new Point(0, 256), 1.0f);
+                    i.DrawImage(image2, new Point(0, kImageSize / 2), 1.0f);
 
                     image3.Mutate(i3 =>
                     {
                         i3.Resize(256, 256);
                     });
-                    i.DrawImage(image3, new Point(256, 256), 1.0f);
+                    i.DrawImage(image3, new Point(kImageSize / 2, kImageSize / 2), 1.0f);
                 });
             });
 
             var imageStream = new MemoryStream();
-            await image.SaveAsync(imageStream, new PngEncoder());
-            imageStream.Position = 0;
+            await image.SaveAsPngAsync(imageStream);
+            if (imageStream.CanSeek)
+            {
+                imageStream.Seek(0, SeekOrigin.Begin);
+            }
             return imageStream;
         }
         
@@ -105,7 +112,7 @@ namespace BeatSaberPlaylistsLib
         /// <returns></returns>
         public static async Task<Stream> GenerateCollage(Stream imageStream1, Stream imageStream2, Stream imageStream3, Stream imageStream4)
         {
-            var image = new Image<Rgba32>(512, 512);
+            var image = new Image<Rgba32>(kImageSize, kImageSize);
             using var image1 = await Image.LoadAsync(imageStream1);
             using var image2 = await Image.LoadAsync(imageStream2);
             using var image3 = await Image.LoadAsync(imageStream3);
@@ -117,33 +124,36 @@ namespace BeatSaberPlaylistsLib
                 {
                     image1.Mutate(i1 =>
                     {
-                        i1.Resize(256, 256);
+                        i1.Resize(kImageSize / 2, kImageSize / 2);
                     });
                     i.DrawImage(image1, Point.Empty, 1.0f);
                 
                     image2.Mutate(i2 =>
                     {
-                        i2.Resize(256, 256);
+                        i2.Resize(kImageSize / 2, kImageSize / 2);
                     });
-                    i.DrawImage(image2, new Point(256, 0), 1.0f);
+                    i.DrawImage(image2, new Point(kImageSize / 2, 0), 1.0f);
                 
                     image3.Mutate(i3 =>
                     {
-                        i3.Resize(256, 256);
+                        i3.Resize(kImageSize / 2, kImageSize / 2);
                     });
-                    i.DrawImage(image3, new Point(0, 256), 1.0f);
+                    i.DrawImage(image3, new Point(0, kImageSize / 2), 1.0f);
                 
                     image4.Mutate(i4 =>
                     {
-                        i4.Resize(256, 256);
+                        i4.Resize(kImageSize / 2, kImageSize / 2);
                     });
-                    i.DrawImage(image4, new Point(256, 256), 1.0f);
+                    i.DrawImage(image4, new Point(kImageSize / 2, kImageSize / 2), 1.0f);
                 });
             });
 
             var imageStream = new MemoryStream();
-            await image.SaveAsync(imageStream, new PngEncoder());
-            imageStream.Position = 0;
+            await image.SaveAsPngAsync(imageStream);
+            if (imageStream.CanSeek)
+            {
+                imageStream.Seek(0, SeekOrigin.Begin);
+            }
             return imageStream;
         }
     }

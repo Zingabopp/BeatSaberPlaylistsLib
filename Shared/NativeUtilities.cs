82f1f8b7c16e5a9bb8558eeea03233c669d81ded
PixelBoom <rithikbhatia12@gmail.com>
2022-03-10 20:11:45 -0500

    Added move to recycle bin option
    The only way to move files to recycling is using a native lib, and it falls back to regular delete if it fails. The method is also not using an optional parameter but is overridden instead, to preserve binary compat.

---
+++ Shared/NativeUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BeatSaberPlaylistsLib
+{
+    internal class NativeUtilities
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SHFILEOPSTRUCT
+        {
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.U4)] public int wFunc;
+            public string pFrom;
+            public string pTo;
+            public short fFlags;
+            [MarshalAs(UnmanagedType.Bool)] public bool fAnyOperationsAborted;
+            public IntPtr hNameMappings;
+            public string lpszProgressTitle;
+        }
+
+        private const int FO_DELETE = 0x0003;
+        private const int FOF_ALLOWUNDO = 0x0040; // Preserve undo information, if possible. 
+        private const int FOF_NOCONFIRMATION = 0x0010; // Show no confirmation dialog box to the user      
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern int SHFileOperation(ref SHFILEOPSTRUCT FileOp);
+
+        public static bool DeleteFileOrFolder(string path)
+        {
+            SHFILEOPSTRUCT fileop = new SHFILEOPSTRUCT();
+            fileop.wFunc = FO_DELETE;
+            fileop.pFrom = path + '\0' + '\0';
+            fileop.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION;
+            
+            var rc = SHFileOperation(ref fileop);
+            return rc == 0;
+        }
+    }
+}
